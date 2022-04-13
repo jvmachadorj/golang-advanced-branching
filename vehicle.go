e205138636ad64cba9545d0d5831aaf30b5831e9
jvmachadorj <jvmachadorj@gmail.com>
2022-04-13 12:10:20 -0300

    commit 2

--- vehicle.go
+++ vehicle.go
@@ -104,16 +104,15 @@ func readJSONFile() Values {
 func generateRating() {
 	f := readJSONFile()
 	for _, v := range f.Models {
-		var vehResult = feedbackResult{}
+		var vehResult feedbackResult
 		var vehRating rating
+
 		for _, msg := range v.Feedback {
-			text := strings.Split(msg, "")
-			if len(text) >= 5 {
+			if text := strings.Split(msg, ""); len(text) >= 5 {
 				vehRating = 5.0
 				vehResult.feedbackTotal++
 				for _, word := range text {
-					s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r")
-					switch s {
+					switch s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"); s {
 					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
 						vehRating += extraPositive
 					case "help", "helpful", "thanks", "thank you", "happy":
